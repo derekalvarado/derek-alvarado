8d8574660d1bd9524f094f34e984ca0d5cf5729b
Derek <dmalvarado@gmail.com>
2016-03-27 21:59:17 -0500

    Added 'thoughts' page. Changed how angular starts up

--- js/app.js
+++ js/app.js
@@ -1,22 +1,10 @@
-(function(){
-  
-  var app = angular.module("myApp", []);
-  
-  
-  
-  var show = function(page) { 
-    
-  }
-  
-  var MainController = function($scope, $log) {
+angular.module("myApp", [])
+.controller("MainController", ['$scope', '$log', function($scope, $log) {
     $scope.page = "about";
-    
-    $scope.show = function(pagestring) {
-      
-      $scope.page = pagestring;
-      
-    }
-  }
 
-  app.controller("MainController", ["$scope", MainController]);
-}());
+    $scope.show = function(pagestring) {
+        
+        $scope.page = pagestring;
+        
+    }
+}]) 
