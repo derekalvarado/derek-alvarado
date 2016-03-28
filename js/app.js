c2a2de5040b1882b0ca1f71b27c9ecbde6e57858
Derek <dmalvarado@gmail.com>
2016-03-27 23:03:11 -0500

    Changed to using ui-router, moved different parts of index.html into partials

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-angular.module("myApp", [])
+angular.module("myApp", ["ui.router"])
+
 .controller("MainController", ['$scope', '$log', function($scope, $log) {
     $scope.page = "about";
 
@@ -7,4 +8,21 @@ angular.module("myApp", [])
         $scope.page = pagestring;
         
     }
-}]) 
+}])
+.config(function($stateProvider, $urlRouterProvider){
+    $urlRouterProvider.otherwise("/about");
+    
+    $stateProvider
+        .state('about', {
+            url: '/about',
+            templateUrl: 'partials/about.html'
+        })
+        .state('contact', {
+            url: '/contact',
+            templateUrl: 'partials/contact.html'
+        })
+        .state('thoughts', {
+            url: '/thoughts',
+            templateUrl: 'partials/thoughts.html'
+        })
+}) 
