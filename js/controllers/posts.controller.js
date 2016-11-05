c8ac9eda258e8bc1f05e931ea7d16f3550b239c1
Derek Alvarado <dmalvarado@gmail.com>
2016-11-05 11:54:31 -0500

    finished getting meta blog post

--- js/controllers/posts.controller.js
+++ js/controllers/posts.controller.js
@@ -1,17 +1,17 @@
 angular.module('myApp')
   .controller('PostsController', PostsController)
 
-PostsController.$inject = ['$log', '$scope', '$state', '$rootScope', '$window']
-function PostsController($log, $scope, $state, $rootScope, $window) {
+PostsController.$inject = ['$log', '$scope', '$state',  '$stateParams','$rootScope', '$window']
+function PostsController($log, $scope, $state, $stateParams, $rootScope, $window) {
   var vm = this;
 
-
+  console.log($stateParams);
 
   $rootScope.$on('$viewContentLoaded', function(e) {
     console.log("viewContentLoaded");
 
     var script = document.createElement('script');
-    script.src = "js/pagemodules/playing-with-canvas.js";
+    script.src = "js/pagemodules/"+$stateParams.name+".js";
     script.async = false;
     document.head.appendChild(script);
 
